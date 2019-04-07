03dbb8ca7172be102d0634c9135a9fd5375bd72b
adr00 <russo21@purdue.edu>
2019-04-07 15:34:39 -0400

    finished person and subclasses

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,2 +1,14 @@
-public class Visitor {
+public class Visitor extends Person{
+
+    public int fare;
+    public boolean paid;
+    public String currentLoc;
+
+    public Visitor(String f, String l, String a, int aa, int ff, boolean p, String c)
+    {
+        super(f,l,a,aa);
+        fare = ff;
+        paid = p;
+        currentLoc = c;
+    }
 }
